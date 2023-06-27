7d959f02dc1844985fcb9e902ae55d6315fabc8e
Khaikhai01 <shishchei@gmail.com>
2023-06-27 22:20:37 +0300

    pcodestyle fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,19 +7,19 @@ def pascal_triangle(n):
     '''pascal triangle'''
 
     if n <= 0:
-        return []  
+        return []
 
-    triangle = []  
+    triangle = []
 
     for i in range(n):
-        row = [1]  
+        row = [1]
         if i > 0:
-            prev_row = triangle[i-1]  
-            for j in range(1, i):  
+            prev_row = triangle[i-1]
+            for j in range(1, i):
                 element = prev_row[j-1] + prev_row[j]
                 row.append(element)
         if i > 0:
-            row.append(1)  
-        triangle.append(row)  
+            row.append(1)
+        triangle.append(row)
 
     return triangle
