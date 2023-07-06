25a9dad1d8e9c55a94527eab33336634cde82db8
Khaikhai01 <shishchei@gmail.com>
2023-07-06 12:36:40 +0300

    pycodestyle fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
+'''returns true of locboxes can all be opened and false if not'''
 
-def join(lst1,lst2):
-  results =[]
-  for num in lst2:
-    results += lst1[num]
-  return results
+
+def join(lst1, lst2):
+    results = []
+    for num in lst2:
+        results += lst1[num]
+    return results
+
 
 def canUnlockAll(boxes):
-  index = 0
-  total = list(set(boxes[0])| {0})
-  added = True
-  while added:
-    added = False
-    for j in join(boxes,total[index:]):
-      if j not in total:
-        total.append(j)
-        index +=1
-        added= True
-  
-  return len(total)==len(boxes)
+    '''returns true of locboxes can all be opened and false if not'''
+    index = 0
+    total = list(set(boxes[0]) | {0})
+    added = True
+    while added:
+        added = False
+        for j in join(boxes, total[index:]):
+            if j not in total:
+                total.append(j)
+                index += 1
+                added = True
+        return len(total) == len(boxes)
